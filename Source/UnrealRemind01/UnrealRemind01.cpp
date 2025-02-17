037f7be8f512e890ce713a5a64df4ccd5932aad6
inhwaman <inhwa424@naver.com>
2025-02-17 19:52:33 +0900

    언리얼 프로젝트 추가
    1주차 및 6번과제를 구현할 언리얼 프로젝트 추가

---
+++ Source/UnrealRemind01/UnrealRemind01.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UnrealRemind01.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UnrealRemind01, "UnrealRemind01" );
